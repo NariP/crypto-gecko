8306fa4b088db791dade83f6645a0ac3b93b3719
narip <narip@github.com>
2024-04-28 17:24:34 +0900

    feat: currency, crypto 관련 공용 함수 추가, 타입 수정

---
+++ src/utils/crypto.ts
@@ -0,0 +1,5 @@
+/**
+ * 심볼을 대문자로 변경하는 함수
+ * - 공용으로 뺀 이유: 심볼은 페이지 내에서 표시될 때 대문자만 사용한다고 가정했기 때문
+ * */
+export const toUpperCoinSymbol = (symbol: string) => symbol.toUpperCase();
---
+++ src/utils/localeCurrency.ts
@@ -0,0 +1,39 @@
+const createNumberFormat = (options?: Omit<Intl.NumberFormatOptions, 'maximumFractionDigits'>) => {
+  return new Intl.NumberFormat(navigator.language || 'ko-KR', {
+    maximumFractionDigits: 2,
+    ...options,
+  });
+};
+
+/** 통화 심볼 표시, 소숫점 두자리까지만 표시 */
+export const localeCurrencySymbol = (price: number, currency = 'krw') => {
+  const formatter = createNumberFormat({
+    style: 'currency',
+    currencyDisplay: 'narrowSymbol',
+    currency: currency.toUpperCase(),
+  });
+
+  return formatter.format(price);
+};
+
+/** 숫자만 표시 */
+export const localeCurrency = (price: number, currency = 'krw') => {
+  const formatter = createNumberFormat({
+    currency: currency.toUpperCase(),
+  });
+
+  return formatter.format(price);
+};
+
+/** 통화 심볼만 표시 */
+export const localeSymbol = (currency = 'krw') => {
+  const formatter = createNumberFormat({
+    style: 'currency',
+    currencyDisplay: 'narrowSymbol',
+    currency: currency.toUpperCase(),
+  });
+
+  const parts = formatter.formatToParts();
+  const currencyPart = parts.find(part => part.type === 'currency');
+  return currencyPart ? currencyPart.value : '';
+};
